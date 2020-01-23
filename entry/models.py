0e4bcdb2a142ea406a98f9223b9b85b2f9d94a2b
blahfacepants <adas@imsa.edu>
2020-01-23 15:58:59 -0600

    added database models

--- entry/models.py
+++ entry/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    winner_score = models.IntegerField()
+    loser_score = models.IntegerField()
+    winner = models.ForeignKey(Player, on_delete=models.CASCADE)
+    loser = models.ForeignKey(Player, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(auto_now=True)
+class Player(models.Model):
+    name = models.CharField(max_length=25)
+    rating = models.IntegerField(default=500)
+    record_wins = models.IntegerField(default=0)
+    record_losses = models.IntegerField(default=0)
+    id = models.IntegerField()
+
